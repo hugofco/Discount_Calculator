94ea334ee5db29679d8b562f56e0b20fbff019e6
Hugo <105819403+hugofco@users.noreply.github.com>
2022-07-09 17:05:21 -0300

    Add files via upload

--- discountcalculator.py
+++ discountcalculator.py
@@ -1,12 +1,15 @@
 import numpy
+import time
 # Product list:
 # potatoes, tomatoes, toilet paper, broom, eggs;
 
-a = "potatoes"
-b = "tomatoes"
-c = "toilet_paper"
-d = "broom"
-e = "eggs"
+print("Write down the name of the products (maximum of 5 )")
+
+a = input("Product1 > ")
+b = input("Product2 > ")
+c = input("Product3 > ")
+d = input("Product4 > ")
+e = input("Product5 > ")
 
 product_list = a, b, c, d, e
 
@@ -22,23 +25,22 @@ code_list = ["A001", "B002", "C003", "D004", "E005"]
 
 # listadepreços
 
-A001_price = 8.78
-B002_price = 5.79
-C003_price = 12.79
-D004_price = 17.98
-E005_price = 9.99
+A001_price = float(input("Product1_price > "))
+B002_price = float(input("Product2_price > "))
+C003_price = float(input("Product3_price > "))
+D004_price = float(input("Product4_price > "))
+E005_price = float(input("Product5_price > "))
 
 price_list = [A001_price, B002_price, C003_price, D004_price, E005_price]
 
 # discount = 27%
 # final price = 100 - Discount =
-Discount = 0.73
-Discount_percentage = '27%'
+Discount = int(input("Discount_value > "))
+Discount2 = 100 - Discount
+Final_discount_value = Discount2/100
+# final response:
 
-# resposta final:
-print('Product Code:')
-print('                  ')
-print(code_list)
+
 print('                  ')
 print('Product List:')
 print('                  ')
@@ -48,24 +50,26 @@ print('Prices(in respective order):')
 print('                  ')
 print(price_list)
 print('                  ')
-print(f'Prices after discount ({Discount_percentage}) respectively:')
+print(f'Prices after discount ({Discount}) respectively:')
 
 Price1 = A001_price
-result1 = numpy.prod(A001_price)*Discount
-print(result1)
+result1 = numpy.prod(A001_price)*Final_discount_value
+print(round(result1, 2))
 
 Price2 = B002_price
-result2 = numpy.prod(B002_price)*Discount
-print(result2)
+result2 = numpy.prod(B002_price)*Final_discount_value
+print(round(result2, 2))
 
 Price3 = C003_price
-result3 = numpy.prod(C003_price)*Discount
-print(result3)
+result3 = numpy.prod(C003_price)*Final_discount_value
+print(round(result3, 2))
 
 Preço4 = D004_price
-result4 = numpy.prod(D004_price)*Discount
-print(result4)
+result4 = numpy.prod(D004_price)*Final_discount_value
+print(round(result4, 2))
 
 Price5 = E005_price
-result5 = numpy.prod(E005_price)*Discount
-print(result5)
+result5 = numpy.prod(E005_price)*Final_discount_value
+print(round(result5, 2))
+
+time.sleep(999)
