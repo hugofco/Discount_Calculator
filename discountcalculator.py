c1651551e6e546c6b72650b7c94040cbc44cc580
Hugo <105819403+hugofco@users.noreply.github.com>
2022-07-12 17:15:53 -0300

    Add files via upload

--- discountcalculator.py
+++ discountcalculator.py
@@ -48,23 +48,18 @@ print(price_list)
 print('')
 print(f'Prices after discount ({Discount_percentage}%) respectively:')
 
-Price1 = A001_price
 result1 = numpy.prod(A001_price)*Final_discount_value
 print(round(result1, 2))
 
-Price2 = B002_price
 result2 = numpy.prod(B002_price)*Final_discount_value
 print(round(result2, 2))
 
-Price3 = C003_price
 result3 = numpy.prod(C003_price)*Final_discount_value
 print(round(result3, 2))
 
-Preço4 = D004_price
 result4 = numpy.prod(D004_price)*Final_discount_value
 print(round(result4, 2))
 
-Price5 = E005_price
 result5 = numpy.prod(E005_price)*Final_discount_value
 print(round(result5, 2))
 
