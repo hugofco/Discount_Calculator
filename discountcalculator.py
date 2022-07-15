683366bd9d2d882045a27d5d65b65a8f3788deec
Hugo <105819403+hugofco@users.noreply.github.com>
2022-07-15 19:00:30 -0300

    Add files via upload

--- discountcalculator.py
+++ discountcalculator.py
@@ -50,43 +50,12 @@ if 5 == operations:
     Price5 = float(input("Product5_price > "))
     PRICES_list = Price1, Price2, Price3, Price4, Price5
 
-# discount = value inserted by user
-
-# final price = 100 - Discount divided by 100
+# Calculating percentage: 100 - (number typed by user), divided by 100
 
 print('')
 Discount_percentage = int(input("Discount_value > "))
 Discount_value = 100 - Discount_percentage
 Final_discount_value = Discount_value/100
-
-# results
-
-if operations == 1:
-    result1 = numpy.prod(Price1)*Final_discount_value
-
-if operations == 2:
-    result1 = numpy.prod(Price1)*Final_discount_value
-    (round(result1, 2))
-    result2 = numpy.prod(Price2)*Final_discount_value
-    print(round(result2, 2))
-
-if operations == 3:
-    result1 = numpy.prod(Price1)*Final_discount_value
-    result2 = numpy.prod(Price2)*Final_discount_value
-    result3 = numpy.prod(Price3)*Final_discount_value
-
-if operations == 4:
-    result1 = numpy.prod(Price1)*Final_discount_value
-    result2 = numpy.prod(Price2)*Final_discount_value
-    result3 = numpy.prod(Price3)*Final_discount_value
-    result4 = numpy.prod(Price4)*Final_discount_value
-
-if operations == 5:
-    result1 = numpy.prod(Price1)*Final_discount_value
-    result2 = numpy.prod(Price2)*Final_discount_value
-    result3 = numpy.prod(Price3)*Final_discount_value
-    result4 = numpy.prod(Price4)*Final_discount_value
-    result5 = numpy.prod(Price5)*Final_discount_value
 
 # final answer:
 
@@ -102,29 +71,46 @@ print('')
 print(f'Prices after discount ({Discount_percentage}%) respectively:')
 print('')
 
+# results
+
 if operations == 1:
+    result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
 
 elif operations == 2:
+    result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
+    result2 = numpy.prod(Price2)*Final_discount_value
     print(round(result2, 2))
 
 elif operations == 3:
+    result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
+    result2 = numpy.prod(Price2)*Final_discount_value
     print(round(result2, 2))
+    result3 = numpy.prod(Price3)*Final_discount_value
     print(round(result3, 2))
 
 elif operations == 4:
+    result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
+    result2 = numpy.prod(Price2)*Final_discount_value
     print(round(result2, 2))
+    result3 = numpy.prod(Price3)*Final_discount_value
     print(round(result3, 2))
-    print(round(result4, 2))
+    result4 = numpy.prod(Price4)*Final_discount_value
+    print(round(result4))
 
 elif operations == 5:
+    result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
+    result2 = numpy.prod(Price2)*Final_discount_value
     print(round(result2, 2))
+    result3 = numpy.prod(Price3)*Final_discount_value
     print(round(result3, 2))
-    print(round(result4, 2))
+    result4 = numpy.prod(Price4)*Final_discount_value
+    print(round(result4))
+    result5 = numpy.prod(Price5)*Final_discount_value
     print(round(result5, 2))
 
 time.sleep(999)
