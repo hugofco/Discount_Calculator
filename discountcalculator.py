e9792873015b4e05e53d239224953e86aa51d7d0
Hugo <105819403+hugofco@users.noreply.github.com>
2022-07-18 22:19:29 -0300

    Add files via upload

--- discountcalculator.py
+++ discountcalculator.py
@@ -71,16 +71,30 @@ print(f'Prices after discount ({Discount_percentage}%) respectively:')
 print('')
 
 # results
+# ev = economized value
 
 if operations == 1:
     result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
+    ev1 = Price1 - result1
+    savings_list = ev1
+    print('')
+    print('Amount saved:')
+    print('')
+    print(round(savings_list, 2))
 
 elif operations == 2:
     result1 = numpy.prod(Price1)*Final_discount_value
     print(round(result1, 2))
     result2 = numpy.prod(Price2)*Final_discount_value
     print(round(result2, 2))
+    ev1 = Price1 - result1
+    ev2 = Price2 - result2
+    savings_list = ev1 + ev2
+    print('')
+    print('Amount saved:')
+    print('')
+    print(round(savings_list, 2))
 
 elif operations == 3:
     result1 = numpy.prod(Price1)*Final_discount_value
@@ -89,6 +103,14 @@ elif operations == 3:
     print(round(result2, 2))
     result3 = numpy.prod(Price3)*Final_discount_value
     print(round(result3, 2))
+    ev1 = Price1 - result1
+    ev2 = Price2 - result2
+    ev3 = Price3 - result3
+    savings_list = ev1 + ev2 + ev3
+    print('')
+    print('Amount saved:')
+    print('')
+    print(round(savings_list, 2))
 
 elif operations == 4:
     result1 = numpy.prod(Price1)*Final_discount_value
@@ -99,6 +121,15 @@ elif operations == 4:
     print(round(result3, 2))
     result4 = numpy.prod(Price4)*Final_discount_value
     print(round(result4))
+    ev1 = Price1 - result1
+    ev2 = Price2 - result2
+    ev3 = Price3 - result3
+    ev4 = Price4 - result4
+    savings_list = ev1 + ev2 + ev3 + ev4
+    print('')
+    print('Amount saved:')
+    print('')
+    print(round(savings_list, 2))
 
 elif operations == 5:
     result1 = numpy.prod(Price1)*Final_discount_value
@@ -111,5 +142,15 @@ elif operations == 5:
     print(round(result4))
     result5 = numpy.prod(Price5)*Final_discount_value
     print(round(result5, 2))
+    ev1 = Price1 - result1
+    ev2 = Price2 - result2
+    ev3 = Price3 - result3
+    ev4 = Price4 - result4
+    ev5 = Price5 - result5
+    savings_list = ev1 + ev2 + ev3 + ev4 + ev5
+    print('')
+    print('Amount saved:')
+    print('')
+    print(round(savings_list, 2))
 
 time.sleep(999)
