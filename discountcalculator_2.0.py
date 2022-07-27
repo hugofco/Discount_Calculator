0ff1031ac2d4e3a86eec4ff1cb45c67fd293d79c
Hugo <105819403+hugofco@users.noreply.github.com>
2022-07-27 10:40:59 -0300

    Add files via upload

--- discountcalculator_2.0.py
+++ discountcalculator_2.0.py
@@ -1,10 +1,6 @@
 import numpy
 import time
 
-# comments if i forget the meaning of some variables:
-# p1 = price1, ev = economized value, dp = Discount percentage,
-# ras = remaining after subtraction, fo = final operator
-
 print()
 while True:
     try:
@@ -28,114 +24,116 @@ print()
 while True:
     try:
         if operations == 1:
-            Prices = p1 = float(input("Product_price> "))
+            prices = price1 = float(input("Product_price> "))
             print()
-            Dp = int(input("Discount percentage > "))
-            ras = 100-Dp
-            Fo = ras/100
+            discount_percentage = int(input("Discount percentage > "))
+            subtracted_value = 100-discount_percentage
+            discount_decimal = subtracted_value/100
             print()
-            print(f'Price after discount ({Dp}%)')
-            result = numpy.prod(Prices)*Fo
+            print(f'Price after discount ({discount_percentage}%)')
+            result = numpy.prod(prices)*discount_decimal
             print(round(result, 2))
-            ev = Prices-result
+            economized_value = prices-result
             print("Amount Saved:")
-            print(round(ev, 2))
+            print(round(economized_value, 2))
 
         elif operations == 2:
-            Prices = p1 = float(input("Product1_price> "))
-            p2 = float(input("Product2_price> "))
+            prices = price1 = float(input("Product1_price> "))
+            price2 = float(input("Product2_price> "))
             print()
-            Dp = int(input("Discount percentage > "))
-            ras = 100-Dp
-            Fo = ras/100
+            discount_percentage = int(input("Discount percentage > "))
+            subtracted_value = 100-discount_percentage
+            discount_decimal = subtracted_value/100
             print()
-            print(f'Prices after discount ({Dp}%)')
-            result1 = numpy.prod(p1)*Fo
+            print(f'Prices after discount ({discount_percentage}%)')
+            result1 = numpy.prod(price1)*discount_decimal
             print(round(result1, 2))
-            result2 = numpy.prod(p2)*Fo
+            result2 = numpy.prod(price2)*discount_decimal
             print(round(result2, 2))
-            ev1 = p1-result1
-            ev2 = p2-result2
+            economized_value1 = price1-result1
+            economized_value2 = price2-result2
             print("Amount Saved:")
-            print(round(ev1+ev2, 2))
+            print(round(economized_value1+economized_value2, 2))
 
         elif operations == 3:
-            Prices = p1 = float(input("Product1_price> "))
-            p2 = float(input("Product2_price> "))
-            p3 = float(input("Product3_price> "))
+            prices = price1 = float(input("Product1_price> "))
+            price2 = float(input("Product2_price> "))
+            price3 = float(input("Product3_price> "))
             print()
-            Dp = int(input("Discount percentage > "))
-            ras = 100-Dp
-            Fo = ras/100
+            discount_percentage = int(input("Discount percentage > "))
+            subtracted_value = 100-discount_percentage
+            discount_decimal = subtracted_value/100
             print()
-            print(f'Prices after discount ({Dp}%)')
-            result1 = numpy.prod(p1)*Fo
+            print(f'Prices after discount ({discount_percentage}%)')
+            result1 = numpy.prod(price1)*discount_decimal
             print(round(result1, 2))
-            result2 = numpy.prod(p2)*Fo
+            result2 = numpy.prod(price2)*discount_decimal
             print(round(result2, 2))
-            result3 = numpy.prod(p3)*Fo
+            result3 = numpy.prod(price3)*discount_decimal
             print(round(result3, 2))
-            ev1 = p1-result1
-            ev2 = p2-result2
-            ev3 = p3-result3
+            economized_value1 = price1-result1
+            economized_value2 = price2-result2
+            economized_value3 = price3-result3
             print("Amount Saved:")
-            print(round(ev1+ev2+ev3, 2))
+            print(round(economized_value1+economized_value2+economized_value3, 2))
 
         elif operations == 4:
-            Prices = p1 = float(input("Product1_price> "))
-            p2 = float(input("Product2_price> "))
-            p3 = float(input("Product3_price> "))
-            p4 = float(input("Product4_price> "))
+            prices = price1 = float(input("Product1_price> "))
+            price2 = float(input("Product2_price> "))
+            price3 = float(input("Product3_price> "))
+            price4 = float(input("Product4_price> "))
             print()
-            Dp = int(input("Discount percentage > "))
-            ras = 100-Dp
-            Fo = ras/100
+            discount_percentage = int(input("Discount percentage > "))
+            subtracted_value = 100-discount_percentage
+            discount_decimal = subtracted_value/100
             print()
-            print(f'Prices after discount ({Dp}%)')
-            result1 = numpy.prod(p1)*Fo
+            print(f'Prices after discount ({discount_percentage}%)')
+            result1 = numpy.prod(price1)*discount_decimal
             print(round(result1, 2))
-            result2 = numpy.prod(p2)*Fo
+            result2 = numpy.prod(price2)*discount_decimal
             print(round(result2, 2))
-            result3 = numpy.prod(p3)*Fo
+            result3 = numpy.prod(price3)*discount_decimal
             print(round(result3, 2))
-            result4 = numpy.prod(p4)*Fo
+            result4 = numpy.prod(price4)*discount_decimal
             print(round(result4, 2))
-            ev1 = p1-result1
-            ev2 = p2-result2
-            ev3 = p3-result3
-            ev4 = p4-result4
+            economized_value1 = price1-result1
+            economized_value2 = price2-result2
+            economized_value3 = price3-result3
+            economized_value4 = price4-result4
             print("Amount Saved:")
-            print(round(ev1+ev2+ev3+ev4, 2))
+            print(round(economized_value1+economized_value2 +
+                  economized_value3+economized_value4, 2))
 
         elif operations == 5:
-            Prices = p1 = float(input("Product1_price> "))
-            p2 = float(input("Product2_price> "))
-            p3 = float(input("Product3_price> "))
-            p4 = float(input("Product4_price> "))
-            p5 = float(input("Product5_price> "))
+            prices = price1 = float(input("Product1_price> "))
+            price2 = float(input("Product2_price> "))
+            price3 = float(input("Product3_price> "))
+            price4 = float(input("Product4_price> "))
+            price5 = float(input("Product5_price> "))
             print()
-            Dp = int(input("Discount percentage > "))
-            ras = 100-Dp
-            Fo = ras/100
+            discount_percentage = int(input("Discount percentage > "))
+            subtracted_value = 100-discount_percentage
+            discount_decimal = subtracted_value/100
             print()
-            print(f'Prices after discount ({Dp}%)')
-            result1 = numpy.prod(p1)*Fo
+            print(f'Prices after discount ({discount_percentage}%)')
+            result1 = numpy.prod(price1)*discount_decimal
             print(round(result1, 2))
-            result2 = numpy.prod(p2)*Fo
+            result2 = numpy.prod(price2)*discount_decimal
             print(round(result2, 2))
-            result3 = numpy.prod(p3)*Fo
+            result3 = numpy.prod(price3)*discount_decimal
             print(round(result3, 2))
-            result4 = numpy.prod(p4)*Fo
+            result4 = numpy.prod(price4)*discount_decimal
             print(round(result4, 2))
-            result5 = numpy.prod(p5)*Fo
+            result5 = numpy.prod(price5)*discount_decimal
             print(round(result5, 2))
-            ev1 = p1-result1
-            ev2 = p2-result2
-            ev3 = p3-result3
-            ev4 = p4-result4
-            ev5 = p5-result5
+            economized_value1 = price1-result1
+            economized_value2 = price2-result2
+            economized_value3 = price3-result3
+            economized_value4 = price4-result4
+            economized_value5 = price5-result5
             print("Amount Saved:")
-            print(round(ev1+ev2+ev3+ev4+ev5, 2))
+            print(round(economized_value1+economized_value2 +
+                  economized_value3+economized_value4+economized_value5, 2))
 
     except ValueError:
         print('Invalid input')
