b57cfdcc3febbaf5d1ce9aef341969c537390f37
Hugo <105819403+hugofco@users.noreply.github.com>
2022-07-28 09:06:37 -0300

    Add files via upload

--- discount_calculator_3.0.py
+++ discount_calculator_3.0.py
@@ -23,83 +23,102 @@ print()
 
 
 def calculate(x):
-    result = numpy.prod(x)*discount_decimal
+    result = numpy.prod(x)*discount
     print(round(result, 2))
+    return(result)
+
+
+def calculate_savings(y, z):
+    economized_value = y-z
+    print(round(economized_value, 2))
+
+
+def ask_for_discount():
+    discount_percentage = int(input("Discount percentage > "))
+    subtracted_value = 100-discount_percentage
+    discount_decimal = subtracted_value/100
+    return(discount_decimal)
 
 
 while True:
     try:
         if operations == 1:
-            prices = price1 = float(input("Product_price> "))
+            price1 = float(input("Product_price> "))
             print()
-            discount_percentage = int(input("Discount percentage > "))
-            subtracted_value = 100-discount_percentage
-            discount_decimal = subtracted_value/100
+            discount = ask_for_discount()
             print()
-            print(f'Price after discount ({discount_percentage}%)')
-            calculate(price1)
+            print("Price after discount:")
+            result1 = calculate(price1)
+            print("Amount saved:")
+            calculate_savings(price1, result1)
 
         elif operations == 2:
-            prices = price1 = float(input("Product1_price> "))
+            price1 = float(input("Product1_price> "))
             price2 = float(input("Product2_price> "))
-            prices = price1+price2
+            prices = price1 + price2
             print()
-            discount_percentage = int(input("Discount percentage > "))
-            subtracted_value = 100-discount_percentage
-            discount_decimal = subtracted_value/100
+            discount = ask_for_discount()
             print()
-            print(f'Prices after discount ({discount_percentage}%)')
-            calculate(price1)
-            calculate(price2)
+            print("Prices after discount:")
+            result1 = calculate(price1)
+            result2 = calculate(price2)
+            results = result1 + result2
+            print("Amount saved:")
+            calculate_savings(prices, results)
 
         elif operations == 3:
-            prices = price1 = float(input("Product1_price> "))
+            price1 = float(input("Product1_price> "))
             price2 = float(input("Product2_price> "))
             price3 = float(input("Product3_price> "))
+            prices = price1 + price2 + price3
             print()
-            discount_percentage = int(input("Discount percentage > "))
-            subtracted_value = 100-discount_percentage
-            discount_decimal = subtracted_value/100
+            discount = ask_for_discount()
             print()
-            print(f'Prices after discount ({discount_percentage}%)')
-            calculate(price1)
-            calculate(price2)
-            calculate(price3)
+            print("Prices after discount:")
+            result1 = calculate(price1)
+            result2 = calculate(price2)
+            result3 = calculate(price3)
+            results = result1 + result2 + result3
+            print("Amount saved:")
+            calculate_savings(prices, results)
 
         elif operations == 4:
             prices = price1 = float(input("Product1_price> "))
             price2 = float(input("Product2_price> "))
             price3 = float(input("Product3_price> "))
             price4 = float(input("Product4_price> "))
+            prices = price1 + price2 + price3 + price4
             print()
-            discount_percentage = int(input("Discount percentage > "))
-            subtracted_value = 100-discount_percentage
-            discount_decimal = subtracted_value/100
+            discount = ask_for_discount()
             print()
-            print(f'Prices after discount ({discount_percentage}%)')
-            calculate(price1)
-            calculate(price2)
-            calculate(price3)
-            calculate(price4)
+            print("Prices after discount:")
+            result1 = calculate(price1)
+            result2 = calculate(price2)
+            result3 = calculate(price3)
+            result4 = calculate(price4)
+            results = result1 + result2 + result3 + result4
+            print("Amount saved:")
+            calculate_savings(prices, results)
 
         elif operations == 5:
-            prices = price1 = float(input("Product1_price> "))
+            price1 = float(input("Product1_price> "))
             price2 = float(input("Product2_price> "))
             price3 = float(input("Product3_price> "))
             price4 = float(input("Product4_price> "))
             price5 = float(input("Product5_price> "))
+            prices = price1 + price2 + price3 + price4 + price5
             print()
-            discount_percentage = int(input("Discount percentage > "))
-            subtracted_value = 100-discount_percentage
-            discount_decimal = subtracted_value/100
+            discount = ask_for_discount()
             print()
-            print(f'Prices after discount ({discount_percentage}%)')
-            print()
-            calculate(price1)
-            calculate(price2)
-            calculate(price3)
-            calculate(price4)
-            calculate(price5)
+            print("Prices after discount:")
+            result1 = calculate(price1)
+            result2 = calculate(price2)
+            result3 = calculate(price3)
+            result4 = calculate(price4)
+            result5 = calculate(price5)
+            results = result1 + result2 + result3 + result4 + result5
+            print("Amount saved:")
+            calculate_savings(prices, results)
 
     except ValueError:
         print('Invalid input')
